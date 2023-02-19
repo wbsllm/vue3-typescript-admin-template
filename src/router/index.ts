83da0dccc3e365a21d573bebf584d5f8233d1704
studio-bao <1727283040@qq.com>
2023-02-19 14:38:31 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import {
   createRouter as createVueRouter,
-  createWebHistory,
+  createWebHashHistory,
   RouteRecordRaw
 } from 'vue-router'
 
@@ -119,7 +119,7 @@ export const asyncRoutes: RouteConfig[] = [
 const createRouter = () =>
   createVueRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
-    history: createWebHistory('element-admin-template-v3-ts'),
+    history: createWebHashHistory('element-admin-template-v3-ts'),
     routes: constantRoutes as any
   })
 
